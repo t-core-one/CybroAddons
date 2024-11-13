9f1773e4b616320a3ea86a1de567a0e72644326b
Cybrosys Technologies <risvana@cybrosys.info>
2024-11-13 16:16:08 +0530

    Nov 13: [ADD] Initial commit 'website_hide_variants'

---
+++ website_hide_variants/__manifest__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Sreeshanth V S (odoo@cybrosys.com)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+##########################################################################
+{
+    'name': "Disable Variants in Website",
+    'version': '18.0.1.0.0',
+    'category': "Website",
+    'summary': "This module is used to hide variants in the website",
+    'description': "The module helps to block to purchase product and "
+                   "product variants from the website based on the condition ",
+    'author': "Cybrosys Techno Solutions",
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['product', 'website_sale', 'sale_management'],
+    'data': ['views/product_views.xml', ],
+    'assets': {
+        'web.assets_frontend': [
+            'website_hide_variants/static/src/js/variants.js'
+        ],
+    },
+    'images': ['static/description/banner.jpg'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ website_hide_variants/doc/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Module <website_hide_variants>
+
+#### 05.11.2024
+#### Version 18.0.1.0.0
+#### ADD
+- Initial commit for Disable Variants in Website
+
