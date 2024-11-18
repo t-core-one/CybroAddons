2620ef899a33de70b75bb32e72eb9160da8a9d8b
Cybrosys Technologies <risvana@cybrosys.info>
2024-11-18 11:02:23 +0530

    Nov 16: [ADD] Initial commit 'membership_card_odoo'

--- membership_card_odoo/__manifest__.py
+++ membership_card_odoo/__manifest__.py
@@ -37,7 +37,7 @@
         'report/membership_card.xml',
         'views/res_partner_views.xml',
     ],
-    'images': ['static/description/banner.jpg'],
+    'images': ['static/description/banner.png'],
     'license': 'AGPL-3',
     'installable': True,
     'auto_install': False,
