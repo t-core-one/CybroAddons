f2622bf1a4d8e1cdd0c16161bebc2212b928b83a
Cybrosys Technologies <risvana@cybrosys.info>
2024-11-26 10:38:11 +0530

    Nov 26: [ADD] Initial commit 'list_tree_pin_records'

---
+++ list_tree_pin_records/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <list_tree_pin_records>
+
+#### 20.11.2024
+#### Version 18.0.1.0.0
+#### ADD
+- Initial commit for Pin Records In Tree/List View
---
+++ list_tree_pin_records/static/src/views/web/pin_records.js
@@ -0,0 +1,58 @@
+import { patch } from "@web/core/utils/patch";
+import { ListRenderer } from "@web/views/list/list_renderer";
+import { onMounted } from "@odoo/owl";
+import { useService } from "@web/core/utils/hooks";
+var array = []
+var int
+
+//Patching ListRenderer
+patch(ListRenderer.prototype, {
+    setup() {
+        super.setup(...arguments);
+        this.orm = useService("orm");
+        onMounted(this.pin)
+    },
+//    pin function to pin records
+    async pin(){
+        var current_model = this.props.list.model.env.searchModel.resModel
+        var table_row = $('.o_data_row')
+        var row = this.props.list.records
+        var result = await this.orm.call('pin.records', 'pin_record',[current_model])
+        if (result){
+            for (var num=0;num<=table_row.length-1;num++){
+                var row_id = row[num].resId
+                for (var i=0;i<=result.length-1;i++){
+                    array.push({'id':result[i].id})
+                     if (row_id == result[i].id){
+                         if(row[num].resModel == result[i].model){
+                            table_row[num].style.setProperty('--table-bg', '#4AEBC8');
+                            table_row[num].parentNode.insertBefore(table_row[num],table_row[0])
+                        }
+                     }
+                }
+            }
+        };
+    },
+    async pin_record_details(ev,record){
+        var row = $(event.target).parent().parent()[0]
+        var num = 0
+        array.forEach(function(line,index){
+            if(line.id == record.resId){
+                row.style.setProperty('--table-bg', 'white');
+                int = index
+                num = 1
+            }
+        });
+        if(num==1){
+            array.splice(int,1)
+            this.actionService.doAction('soft_reload');
+        }
+        if(num==0) {
+            $($(event.target).parent().parent().parent()[0]).prepend(row)
+            array.push({'id': record.resId})
+            row.style.setProperty('--table-bg', '#4AEBC8');
+        }
+        var self = this;
+        var result = await this.orm.call('pin.records', 'save_pin_record',[[parseInt(record.resId),record.resModel,row.style.backgroundColor]])
+    }
+});
