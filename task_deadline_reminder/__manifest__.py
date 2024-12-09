4febea5a3e9ea12476bf5e5ccacc8d8d87ce1280
Cybrosys Technologies <risvana@cybrosys.info>
2024-12-09 16:10:38 +0530

    Dec 09: [UPDT] Manifest Updated 'task_deadline_reminder'

--- task_deadline_reminder/__manifest__.py
+++ task_deadline_reminder/__manifest__.py
@@ -37,7 +37,7 @@
         'data/ir_cron_data.xml',
         'data/mail_template_data.xml',
     ],
-    'images': ['static/description/banner.png'],
+    'images': ['static/description/banner.jpg'],
     'license': 'AGPL-3',
     'installable': True,
     'auto_install': False,
